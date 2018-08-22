187e7167926bc9c7077f0e9e2403517c2ad80583
cumibulat <juwandysusilo@gmail.com>
2018-08-22 17:01:13 +0700

    progress on dashboard

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,10 +1,4 @@
 import { injectGlobal } from 'styled-components';
 
 /* eslint no-unused-expressions: 0 */
-injectGlobal`
-  html,
-  body {
-    height: 100%;
-    width: 100%;
-  }
-`;
+injectGlobal``;
